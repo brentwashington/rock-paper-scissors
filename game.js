5f003ec84e63c3a2a370f6ca77643caea550f4a3
Brent <brentwashing@gmail.com>
2025-06-12 19:25:32 -0500

    Create get computer choice function

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+let number = 0;
+let choice = "";
+
+function getComputerChoice() {
+    // Generate a random number between 0 and 2
+    number = Math.floor(Math.random() * 3);
+    switch (number) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper"
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+        default:
+            console.log("The computer seems to be indecisive!")
+    }
+
+    return choice;
+}
